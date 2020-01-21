7d9313f8d26f37963794ac478674709748c963fc
Nikolai Imbirev <nikolai_imbirev@epam.com>
2020-01-21 15:44:03 +0300

    add classes javadocs

--- src/main/java/com/epam/lemon/exception/RecordLengthException.java
+++ src/main/java/com/epam/lemon/exception/RecordLengthException.java
@@ -1,6 +1,15 @@
 package com.epam.lemon.exception;
 
+/**
+ * The exception class for situations, where the record length of the dataset is invalid.
+ */
 public class RecordLengthException extends RuntimeException {
+
+    /**
+     * Main exception constructor.
+     * The following message, which will be generated will show the expected record length
+     * @param expectedLength the expected length of the record
+     */
     public RecordLengthException(Integer expectedLength) {
         super("Record length is incorrect: should be " + expectedLength);
     }
